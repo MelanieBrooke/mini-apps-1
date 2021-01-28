72214b06d0008086a402bc01d03279e3677fd6fd
Melanie DeWitt <melaniebd1986@gmail.com>
2021-01-27 22:08:33 -0700

    Attempting to connect server and client for CSV challenge

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+app.use(express.static('client'));
 
 
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+})
+
+
+
+
+
+// routers
+
+app.post('/' (req, res) => {
+  // preventDefault();
+  console.log('hi');
+  console.log(req.body);
+});
