c17be845f8abdbe90267ebbce93745244c24e524
Melanie DeWitt <melaniebd1986@gmail.com>
2021-02-13 22:00:23 -0700

    Begin setup

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const app = express();
+require("dotenv").config();
+let port = process.env.PORT;
 
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`)
+})
 
