8042d39ac7bf0b5f886680fcf278e3c523ab9e24
Melanie DeWitt <melaniebd1986@gmail.com>
2021-02-17 12:59:58 -0700

    dropping alternating chips into board

--- challenge_4/client/src/components/Board.jsx
+++ challenge_4/client/src/components/Board.jsx
@@ -4,11 +4,68 @@ const Board = (props) => {
   return(
     <div>
       <table id="gameboard">
+      <tr id="columns" onClick={props.onClick}>
+        <th id="a" name="a"></th>
+        <th id="b" name="b"></th>
+        <th id="c" name="c"></th>
+        <th id="d" name="d"></th>
+        <th id="e" name="e"></th>
+        <th id="f" name="f"></th>
+        <th id="g" name="g"></th>
+      </tr>
+      <tr id="6">
+          <td><div id="a6" class="a 6 white"></div></td>
+          <td><div id="b6" class="b 6 white"></div></td>
+          <td><div id="c6" class="c 6 white"></div></td>
+          <td><div id="d6" class="d 6 white"></div></td>
+          <td><div id="e6" class="e 6 white"></div></td>
+          <td><div id="f6" class="f 6 white"></div></td>
+          <td><div id="g6" class="g 6 white"></div></td>
+        </tr>
+      <tr id="5">
+          <td><div id="a5" class="a 5 white"></div></td>
+          <td><div id="b5" class="b 5 white"></div></td>
+          <td><div id="c5" class="c 5 white"></div></td>
+          <td><div id="d5" class="d 5 white"></div></td>
+          <td><div id="e5" class="e 5 white"></div></td>
+          <td><div id="f5" class="f 5 white"></div></td>
+          <td><div id="g5" class="g 5 white"></div></td>
+        </tr>
+      <tr id="4">
+          <td><div id="a4" class="a 4 white"></div></td>
+          <td><div id="b4" class="b 4 white"></div></td>
+          <td><div id="c4" class="c 4 white"></div></td>
+          <td><div id="d4" class="d 4 white"></div></td>
+          <td><div id="e4" class="e 4 white"></div></td>
+          <td><div id="f4" class="f 4 white"></div></td>
+          <td><div id="g4" class="g 4 white"></div></td>
+        </tr>
+      <tr id="3">
+          <td><div id="a3" class="a 3 white"></div></td>
+          <td><div id="b3" class="b 3 white"></div></td>
+          <td><div id="c3" class="c 3 white"></div></td>
+          <td><div id="d3" class="d 3 white"></div></td>
+          <td><div id="e3" class="e 3 white"></div></td>
+          <td><div id="f3" class="f 3 white"></div></td>
+          <td><div id="g3" class="g 3 white"></div></td>
+        </tr>
       <tr id="2">
-          <td id="a2">()</td><td id="b2">()</td><td id="c2">()</td><td id="d2">()</td><td id="e2">()</td><td id="f2">()</td><td id="g2">()</td>
+          <td><div id="a2" class="a 2 white"></div></td>
+          <td><div id="b2" class="b 2 white"></div></td>
+          <td><div id="c2" class="c 2 white"></div></td>
+          <td><div id="d2" class="d 2 white"></div></td>
+          <td><div id="e2" class="e 2 white"></div></td>
+          <td><div id="f2" class="f 2 white"></div></td>
+          <td><div id="g2" class="g 2 white"></div></td>
         </tr>
         <tr id="1">
-          <td id="a1">()</td><td id="b1">()</td><td id="c1">()</td><td id="d1">()</td><td id="e1">()</td><td id="f1">()</td><td id="g1">()</td>
+          <td><div id="a1" class="a 1 white"></div></td>
+          <td><div id="b1" class="b 1 white"></div></td>
+          <td><div id="c1" class="c 1 white"></div></td>
+          <td><div id="d1" class="d 1 white"></div></td>
+          <td><div id="e1" class="e 1 white"></div></td>
+          <td><div id="f1" class="f 1 white"></div></td>
+          <td><div id="g1" class="g 1 white"></div></td>
         </tr>
       </table>
     </div>
--- challenge_4/client/src/index.jsx
+++ challenge_4/client/src/index.jsx
@@ -6,17 +6,172 @@ import '../dist/style.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      player: "red",
+      pieces: {
+        a: {
+          1:{empty:true, color:"white"},
+          2:{empty:true, color:"white"},
+          3:{empty:true, color:"white"},
+          4:{empty:true, color:"white"},
+          5:{empty:true, color:"white"},
+          6:{empty:true, color:"white"},
+        },
+        b: {
+          1:{empty:true, color:"white"},
+          2:{empty:true, color:"white"},
+          3:{empty:true, color:"white"},
+          4:{empty:true, color:"white"},
+          5:{empty:true, color:"white"},
+          6:{empty:true, color:"white"},
+        },
+        c: {
+          1:{empty:true, color:"white"},
+          2:{empty:true, color:"white"},
+          3:{empty:true, color:"white"},
+          4:{empty:true, color:"white"},
+          5:{empty:true, color:"white"},
+          6:{empty:true, color:"white"},
+        },
+        d: {
+          1:{empty:true, color:"white"},
+          2:{empty:true, color:"white"},
+          3:{empty:true, color:"white"},
+          4:{empty:true, color:"white"},
+          5:{empty:true, color:"white"},
+          6:{empty:true, color:"white"},
+        },
+        e: {
+          1:{empty:true, color:"white"},
+          2:{empty:true, color:"white"},
+          3:{empty:true, color:"white"},
+          4:{empty:true, color:"white"},
+          5:{empty:true, color:"white"},
+          6:{empty:true, color:"white"},
+        },
+        f: {
+          1:{empty:true, color:"white"},
+          2:{empty:true, color:"white"},
+          3:{empty:true, color:"white"},
+          4:{empty:true, color:"white"},
+          5:{empty:true, color:"white"},
+          6:{empty:true, color:"white"},
+        },
+        g: {
+          1:{empty:true, color:"white"},
+          2:{empty:true, color:"white"},
+          3:{empty:true, color:"white"},
+          4:{empty:true, color:"white"},
+          5:{empty:true, color:"white"},
+          6:{empty:true, color:"white"},
+        },
+      }
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.checkColumnForEmpty = this.checkColumnForEmpty.bind(this);
+    this.dropPiece = this.dropPiece.bind(this);
+    this.togglePlayer = this.togglePlayer.bind(this);
+    this.declareWin = this.declareWin.bind(this);
+    this.detectColumnWin = this.detectColumnWin.bind(this);
+    this.detectRowWin = this.detectRowWin.bind(this);
+    this.detectMajorDiagonalWin = this.detectMajorDiagonalWin.bind(this);
+    this.detectMinorDiagonalWin = this.detectMinorDiagonalWin.bind(this);
   }
 
   render() {
     return(
       <div>
-        <Board />
+        <h1>Connect Four</h1>
+        <h3>It's {this.state.player} player's turn.</h3>
+        <Board onClick={this.handleClick}/>
       </div>
     )
   }
+
+  handleClick(event) {
+    event.preventDefault();
+    console.log(event.target.id);
+    this.checkColumnForEmpty(event.target.id)
+  }
+
+  checkColumnForEmpty (row) {
+    var currentColumn = this.state.pieces[row];
+    var full = true;
+    for (var i = 1; i < 7; i++) {
+      if (currentColumn[i].empty === true) {
+        this.state.pieces[row][i].empty = false;
+        this.state.pieces[row][i].color = this.state.player;
+        console.log(this.state.pieces[row]);
+        full = false;
+        var piece = row.toString() + i.toString();
+        this.dropPiece(piece);
+        break;
+      }
+    }
+    if (full === true) {
+      alert('This column is full!')
+    }
+  }
+
+  dropPiece(space) {
+    document.getElementById(space).classList.add(this.state.player.toString())
+    document.getElementById(space).classList.remove("white");
+    this.togglePlayer();
+  };
+
+  togglePlayer() {
+    if (this.state.player === "red") {
+      this.state.player = "black";
+    } else {
+      this.state.player = "red";
+    }
+    this.setState({
+      player: this.state.player
+    })
+  }
+
+  detectColumnWin() {
+    for (var col in this.state.pieces) {
+      for (var j = 1; j < 4; j++) {
+        if (col[j].empty === false &&
+          col[j+1].empty === false &&
+          col[j+2].empty === false &&
+          col[j+3].empty === false) {
+          if (col[j].color === col[j+1].color &&
+            col[j].color === col[j+2].color &&
+            col[j].color === col[j+3].color) {
+            this.declareWin();
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  detectRowWin() {
+
+  }
+
+  detectMajorDiagonalWin() {
+
+  }
+
+  detectMinorDiagonalWin() {
+
+  }
+
+  declareWin() {
+    console.log('Win!')
+  }
+
 }
+
+
+
+
+
+
+
 
 ReactDOM.render(<App/>, document.getElementById('app'));
 
